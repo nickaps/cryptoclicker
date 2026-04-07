8bec3b60ae5119d66a8e1d834cbfa11079e9e020
nick <nickisepic8@gmail.com>
2026-04-07 14:12:02 -0500

    initial commit

---
+++ src/main/java/edu/uwrf/se/cryptoclicker/cryptoclicker/CryptoclickerApplication.java
@@ -0,0 +1,13 @@
+package edu.uwrf.se.cryptoclicker.cryptoclicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoclickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoclickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uwrf/se/cryptoclicker/cryptoclicker/CryptoclickerApplicationTests.java
@@ -0,0 +1,14 @@
+package edu.uwrf.se.cryptoclicker.cryptoclicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoclickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
