0b3ae6d1821d387def82e959983e43b9b14567df
ted <theodore.robert2003@gmail.com>
2026-04-27 15:18:53 -0500

    added the propper hover properties to the welcome buttons built upon nicks clicker code - Added a clicker counter and score display

--- src/main/resources/static/scripts/game.js
+++ src/main/resources/static/scripts/game.js
@@ -1,10 +1,17 @@
 
 var score = 0;
 
+
+
 function exitGame() {
     window.location.href = "/exit?newScore=" + Number(score);
 }
 
 function addScore() {
     score += 1;
+    console.log(score);
+    let scoreDisplay = document.getElementById("score");
+    //element.innerHTML = element.innerHTML.replace("score", score);
+    scoreDisplay.textContent = score;
+    //document.getElementById.replace("0", score);
 }
